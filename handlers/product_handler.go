be3f9524cb3c0b2675a0567eb637d9c96e2f7e96
hanasbp <hanasoke@gmail.com>
2025-12-01 08:57:39 +0700

    added handler file & folder in the project

---
+++ handlers/product_handler.go
@@ -0,0 +1,13 @@
+package handlers
+
+import (
+	"electric-store/models"
+)
+
+type ProductHandler struct {
+	model *models.ProductModel
+}
+
+func NewProductHandler() (*ProductHandler, error) {
+
+}
