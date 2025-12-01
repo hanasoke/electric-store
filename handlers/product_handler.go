0c0e2d9a7c1e698812d4a90f96a5d1f576373b1e
hanasbp <hanasoke@gmail.com>
2025-12-01 10:29:25 +0700

    adding EditForm function in product_handler.go

--- handlers/product_handler.go
+++ handlers/product_handler.go
@@ -5,6 +5,7 @@ import (
 	"electric-store/models"
 	"html/template"
 	"net/http"
+	"strconv"
 )
 
 type ProductHandler struct {
@@ -45,4 +46,59 @@ func (ph *ProductHandler) CreateForm(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	tmpl.Execute(w, nil)
+}
+
+func (ph *ProductHandler) Create(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	price, _ := strconv.ParseFloat(r.FormValue("price"), 64)
+	stock, _ := strconv.Atoi(r.FormValue("stock"))
+
+	product := &models.Product{
+		Name:        r.FormValue("name"),
+		Category:    r.FormValue("category"),
+		Price:       price,
+		Stock:       stock,
+		Description: r.FormValue("description"),
+	}
+
+	err = ph.model.Create(product)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func (ph *ProductHandler) EditForm(w http.ResponseWriter, r *http.Request) {
+	idStr := r.URL.Query().Get("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid product ID", http.StatusBadRequest)
+		return
+	}
+
+	product, err := ph.model.GetByID(id)
+	if err != nil {
+		http.Error(w, "Product not found", http.StatusNotFound)
+		return
+	}
+
+	tmpl, err := template.ParseFiles("templates/edit.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	tmpl.Execute(w, product)
 }
