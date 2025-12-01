6e7a26b7874a9bf4ac0f69031a8f038435c01c9b
hanasbp <hanasoke@gmail.com>
2025-12-01 10:40:07 +0700

    added Update & Delete Functions in product_handler.go

--- handlers/product_handler.go
+++ handlers/product_handler.go
@@ -102,3 +102,54 @@ func (ph *ProductHandler) EditForm(w http.ResponseWriter, r *http.Request) {
 
 	tmpl.Execute(w, product)
 }
+
+func (ph *ProductHandler) Update(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	id, _ := strconv.Atoi(r.FormValue("id"))
+	price, _ := strconv.ParseFloat(r.FormValue("price"), 64)
+	stock, _ := strconv.Atoi(r.FormValue("stock"))
+
+	product := &models.Product{
+		ID:          id,
+		Name:        r.FormValue("name"),
+		Category:    r.FormValue("category"),
+		Price:       price,
+		Stock:       stock,
+		Description: r.FormValue("description"),
+	}
+
+	err = ph.model.Update(product)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func (ph *ProductHandler) Delete(w http.ResponseWriter, r *http.Request) {
+	idStr := r.URL.Query().Get("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid product ID", http.StatusBadRequest)
+		return
+	}
+
+	err = ph.model.Delete(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
