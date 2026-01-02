3d16b829c2ca9a716be4617dc3aa17d7e6439118
hanasbp <hanasoke@gmail.com>
2026-01-02 09:04:43 +0700

    updated category & product pages

--- controllers/categorycontroller/categorycontroller.go
+++ controllers/categorycontroller/categorycontroller.go
@@ -1,1 +1,15 @@
 package categorycontroller
+
+import (
+	"net/http"
+	"text/template"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	temp, err := template.ParseFiles("views/categories/index.html")
+	if err != nil {
+		panic(err)
+	}
+
+	temp.Execute(w, nil)
+}
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"electric-store/config"
+	"electric-store/controllers/categorycontroller"
 	"electric-store/controllers/productcontroller"
 	"log"
 	"net/http"
@@ -11,6 +12,7 @@ func main() {
 	config.ConnectDB()
 
 	http.HandleFunc("/", productcontroller.Index)
+	http.HandleFunc("/categories", categorycontroller.Index)
 
 	log.Println("Server running on port 7050")
 	http.ListenAndServe(":7050", nil)
