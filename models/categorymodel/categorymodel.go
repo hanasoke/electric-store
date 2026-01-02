b5aebb3f512e69147af311421dea58332ccecf99
hanasbp <hanasoke@gmail.com>
2026-01-02 11:40:58 +0700

    updated categorymodel.go

--- models/categorymodel/categorymodel.go
+++ models/categorymodel/categorymodel.go
@@ -3,6 +3,7 @@ package categorymodel
 import (
 	"database/sql"
 	"electric-store/config"
+	"electric-store/entities"
 	"errors"
 )
 
@@ -24,4 +25,25 @@ func IsCategoryExists(name string) (bool, error) {
 	}
 
 	return true, nil
+}
+
+func GetAll() []entities.Category {
+	rows, err := config.DB.Query("SELECT * FROM categories")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	var categories []entities.Category
+
+	for rows.Next() {
+		var category entities.Category
+		if err != rows.Scan(&category.Id, &category.Name, &category.CreatedAt, category.UpdatedAt) {
+			return nil
+		}
+		categories = append(categories, category)
+	}
+
+	return categories
+
 }
