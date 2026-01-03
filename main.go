9b3e0fc0f9c97f7de9647551284965fba72f7763
hanasbp <hanasoke@gmail.com>
2026-01-03 10:53:56 +0700

    added add category route

--- main.go
+++ main.go
@@ -21,6 +21,7 @@ func main() {
 
 	http.HandleFunc("/", productcontroller.Index)
 	http.HandleFunc("/categories", categorycontroller.Index)
+	http.HandleFunc("/category/add", categorycontroller.Add)
 
 	log.Println("Server running on port 7050")
 	http.ListenAndServe(":7050", nil)
