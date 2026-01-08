8de9b13c232439e13d995b00a6b068a4b25e9972
hanasbp <hanasoke@gmail.com>
2026-01-08 07:34:30 +0700

    added basic of index.html & productcontroller.go

--- controllers/productcontroller/productcontroller.go
+++ controllers/productcontroller/productcontroller.go
@@ -1,8 +1,19 @@
 package productcontroller
 
 import (
+	"electric-store/controllers"
 	"net/http"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
+	// prepare template data
+	data := struct {
+		Title      string
+		ActivePage string
+	}{
+		Title:      "Products",
+		ActivePage: "products",
+	}
+
+	controllers.RenderTemplate(w, "products", data)
 }
