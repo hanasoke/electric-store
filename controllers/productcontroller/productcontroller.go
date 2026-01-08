358d42f4c5d5030d8d65ea7bdd7fc0c21690e7f7
hanasbp <hanasoke@gmail.com>
2026-01-08 08:04:40 +0700

    updated Index controller in productcontroller.go

--- controllers/productcontroller/productcontroller.go
+++ controllers/productcontroller/productcontroller.go
@@ -2,17 +2,28 @@ package productcontroller
 
 import (
 	"electric-store/controllers"
+	"electric-store/entities"
+	"electric-store/models/productmodel"
 	"net/http"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
+	// Get all products from database
+	products, err := productmodel.GetAll()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	// prepare template data
 	data := struct {
 		Title      string
 		ActivePage string
+		Products   []entities.Product
 	}{
 		Title:      "Products",
 		ActivePage: "products",
+		Products:   products,
 	}
 
 	controllers.RenderTemplate(w, "products", data)
